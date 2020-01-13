8cb4e1a7f7282bb678ee7ec7edc56745e3f663a2
BIll <billfredtom@gmail.com>
2020-01-13 22:06:24 +1100

    added unit test project with small tests

---
+++ nib.techChallenge.BillThomas.tests/OrderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nib.TechChallenge.BillThomas;
+
+namespace nib.TechChallenge.BillThomas.tests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void UnfulfilledOrders()
+        {
+            List<Order> oList = new List<Order>();
+
+            for (int i = 1; i < 4; i++)
+            {
+                Order oNew = new Order();
+                oNew.OrderId = i;
+                oNew.Status = "pending";
+                oNew.DateCreated = System.DateTime.Now;
+
+                oList.Add(oNew);
+            }
+
+            Order orderReport = new Order();
+            string x = orderReport.unforfilledReport(oList);
+
+     
+        }
+    }
+}
---
+++ nib.techChallenge.BillThomas.tests/ProductTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nib.TechChallenge.BillThomas;
+
+namespace nib.TechChallenge.BillThomas.tests
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void SelectExistingProductById()
+        {
+            //
+            List<Product> pList = new List<Product>();
+           
+            for(int i = 1; i < 4; i++)
+            {
+                Product pNew = new Product();
+                pNew.ProductId = i;
+                pNew.Description = "description " + i;
+                pNew.DeliveryLeadTime = 8;
+                pNew.QuantityOnHand = 8;
+                pNew.ReorderThreshold = 8;
+                pNew.ReorderAmount = 8;
+
+                pList.Add(pNew);
+            }
+
+            Product pSelectTest = new Product();
+            pSelectTest.SelectByProductId(2, pList); 
+        }
+    }
+}
